4493f02b55e4c035acc5cabc30019b2ddcd327b9
Steven H <stevenhuynh105@gmail.com>
2026-03-13 22:21:12 -0700

    Linear Probing - Access Operator

--- TextGenerator/include/LinearProbing.hpp
+++ TextGenerator/include/LinearProbing.hpp
@@ -149,7 +149,9 @@ template <typename Key, typename Value>
 Value& LinearProbing<Key, Value>::operator[](const Key& key) {
     // TODO: implement operator[] to return reference to value associated with key
     // if key not found, insert default-constructed Value and return its reference
-
+    if (find(key)) { return *find(key); }
+    insert(key, Value{});
+    return *find(key);
 }
 
 template <typename Key, typename Value>
